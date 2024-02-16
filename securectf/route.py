ce5e7ee7ecafc008d392aee9f4b9762c2ae5c213
abhijit <abhijitnyk001@gmail.com>
2024-02-16 20:38:22 +0530

    added all the relevent routes

--- securectf/route.py
+++ securectf/route.py
@@ -15,3 +15,15 @@ def signup():
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/ctf')
+def ctf():
+    return render_template('ctf.html')
+
+@app.route('/forum')
+def forum():
+    return render_template('forum.html')
+
+@app.route('/community')
+def community():
+    return render_template('community.html')
