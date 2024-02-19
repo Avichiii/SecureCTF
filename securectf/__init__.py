f498d32de2ba0c1698ae4a319626afbe526c51a5
abhijit <abhijitnyk001@gmail.com>
2024-02-19 21:26:50 +0530

    login_manager instance __init__

--- securectf/__init__.py
+++ securectf/__init__.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '9b02d00476969f0e04ca626be846d4'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///securectf.db'
 db = SQLAlchemy(app)
-
+login_manager = LoginManager(app)
 from securectf import route
