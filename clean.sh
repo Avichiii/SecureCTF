9008058aa80b3065316c3eaeb9697c039713191d
abhijit <abhijitnyk001@gmail.com>
2024-02-21 11:08:37 +0530

    update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
-if [[ -d ".vscode-root" ]]
+if [[ -d "~.vscode-root" ]]
 then 
     git rm -rf --cached .vscode-root
-    rm -rf .vscode-root
+    rm -rf ~.vscode-root
 else
-    git rm -rf --cached "*Cache*"
-    git rm -rf --cached logs
-    git rm -rf --cached Dictionaries
-    git rm -rf --cached databases
-    git rm -rf --cached "Service Worker"
-    git rm -rf --cached User
-    git rm -rf --cached "*Storage*"
-    git rm -rf --cached "*Cookies*"
-    git rm -rf --cached "*Trust*"
-    git rm --cached Preferences
-    git rm -rf --cached "Network*"
-    git rm -rf --cached Crashpad
+    removeList=("*Cache*" logs Dictionaries databases "Service Worker" User "*Storage*" "*Cookies*" "*Trust*" Preferences "Network*" Crashpad)
+    for del in ${removeList[*]}
+    do
+        git rm -rf --cached $del
+        rm -rm $del
+    done
 fi
 
